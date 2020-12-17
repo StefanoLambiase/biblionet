0811b77aec00065cb04403af7ceca152de0f36bc
Stefano Lambiase <stefanolambiase96@gmail.com>
2020-12-17 18:46:04 +0100

    Initial commit

---
+++ src/test/java/it/unisa/c07/biblionet/BiblionetApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unisa.c07.biblionet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiblionetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
