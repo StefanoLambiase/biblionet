1c79758c4affc1a5aa1b5dccd97a80a9e310f4fe
Tekhorror <ciromaiorino99@gmail.com>
2020-12-18 16:49:11 +0100

    Aggiunte annotazioni alle entity
    Aggiunte notazioni alle entity Esperto e Biblioteca

--- src/main/java/it/unisa/c07/biblionet/model/entity/utente/Biblioteca.java
+++ src/main/java/it/unisa/c07/biblionet/model/entity/utente/Biblioteca.java
@@ -4,11 +4,15 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 @Data
 @NoArgsConstructor
 public class Biblioteca extends UtenteRegistrato{
+    @NotNull
+    @Size(max = 30)
     String nomeBiblioteca;
 
     public Biblioteca(String email, String password, String provincia, String citta, String via, String recapitoTelefonico, String nomeBiblioteca) {
