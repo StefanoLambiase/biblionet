9e226c25fd153a8a296f0e47bcd71b1ab8216b28
yabok <gianmario98@live.it>
2020-12-19 15:46:22 +0100

    Creazione package Club del libro

---
+++ src/main/java/it/unisa/c07/biblionet/clubDelLibro/controller/CreazioneClubControl.java
@@ -0,0 +1,4 @@
+package it.unisa.c07.biblionet.clubDelLibro.controller;
+
+public class CreazioneClubControl {
+}
---
+++ src/main/java/it/unisa/c07/biblionet/clubDelLibro/service/ClubDeliLibroService.java
@@ -0,0 +1,4 @@
+package it.unisa.c07.biblionet.clubDelLibro.service;
+
+public interface ClubDeliLibroService {
+}
