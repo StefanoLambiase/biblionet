9a7fd5d67917be62bb43b84883b16e9b1e8be9f9
Tekhorror <ciromaiorino99@gmail.com>
2020-12-20 17:35:12 +0100

    Macavano i punti,errore mio

--- src/main/java/it/unisa/c07/biblionet/utils/Length.java
+++ src/main/java/it/unisa/c07/biblionet/utils/Length.java
@@ -7,42 +7,42 @@ package it.unisa.c07.biblionet.utils;
 public abstract class Length {
 
     /**
-     * Lunghezza 13
+     * Lunghezza 13.
      */
     public static final int LENGTH_13 = 13;
 
     /**
-     * Lunghezza 30
+     * Lunghezza 30.
      */
     public static final int LENGTH_30 = 30;
 
     /**
-     * Lunghezza 32
+     * Lunghezza 32.
      */
     public static final int LENGTH_32 = 32;
 
     /**
-     * Lunghezza 60
+     * Lunghezza 60.
      */
     public static final int LENGTH_60 = 60;
 
     /**
-     * Lunghezza 90
+     * Lunghezza 90.
      */
     public static final int LENGTH_90 = 90;
 
     /**
-     * Lunghezza 255
+     * Lunghezza 255.
      */
     public static final int LENGTH_255 = 255;
 
     /**
-     * Lunghezza 144
+     * Lunghezza 144.
      */
     public static final int LENGTH_144 = 144;
 
     /**
-     * Lunghezza 320
+     * Lunghezza 320.
      */
     public static final int LENGTH_320 = 320;
 
