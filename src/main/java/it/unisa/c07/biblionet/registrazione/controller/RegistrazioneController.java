3d0f962d621c28785629747b89f68415313f3ef1
Antonio <57596737+atdepo@users.noreply.github.com>
2020-12-21 17:22:42 +0100

    implementazione scelta e inizio implementazione lettore
    @BearGrillz99

--- src/main/java/it/unisa/c07/biblionet/registrazione/controller/RegistrazioneController.java
+++ src/main/java/it/unisa/c07/biblionet/registrazione/controller/RegistrazioneController.java
@@ -1,12 +1,16 @@
 package it.unisa.c07.biblionet.registrazione.controller;
 
 import it.unisa.c07.biblionet.model.entity.utente.Lettore;
+import it.unisa.c07.biblionet.model.entity.utente.UtenteRegistrato;
 import it.unisa.c07.biblionet.registrazione.service.RegistrazioneService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Locale;
 
 @Controller
 @RequiredArgsConstructor
@@ -14,16 +18,23 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class RegistrazioneController {
     private final RegistrazioneService registrazioneService;
 
-    @RequestMapping(value = "/", method = RequestMethod.POST)
+    @RequestMapping(value = "/", method = RequestMethod.GET)
     public String visualizzaRegistrazione() {
-        return "lettore";
+        return "registrazione";
+    }
+
+    @RequestMapping(value = "/scegli", method = RequestMethod.GET)
+    public String scegliRegistrazione(final @RequestParam("scelta") String scelta) {
+        System.out.println(scelta);
+        return "redirect:/registrazione/"+scelta.toLowerCase();
     }
 
 
-    @RequestMapping(value = "/lettore", method = RequestMethod.POST)
+
+    @RequestMapping(value = "/lettore", method = RequestMethod.GET)
     public String registrazioneLettore(final Lettore lettore){
         System.out.println(lettore);
-        return "registrazione";
+        return "registrazione_lettore";
     }
 
 }
