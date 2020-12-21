321fcab9fb70e8650cd4fa29a3e14c7e18b707fd
Tekhorror <ciromaiorino99@gmail.com>
2020-12-21 16:50:11 +0100

    Inizio controller

--- src/main/java/it/unisa/c07/biblionet/autenticazione/controller/AutenticazioneController.java
+++ src/main/java/it/unisa/c07/biblionet/autenticazione/controller/AutenticazioneController.java
@@ -1,4 +1,15 @@
 package it.unisa.c07.biblionet.autenticazione.controller;
 
+import it.unisa.c07.biblionet.autenticazione.service.AutenticazioneService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/login")
 public class AutenticazioneController {
+    private final AutenticazioneService autenticazioneService;
+
+    
 }
