eda1ab6e9e508d7571103053ca684c7989875933
Antonio <57596737+atdepo@users.noreply.github.com>
2020-12-21 19:16:27 +0100

    Preparazione package per test
    Co-Authored-By: Alessio Casolaro <65906352+BearGrillz99@users.noreply.github.com>

---
+++ src/test/java/it/unisa/c07/biblionet/registrazione/controller/RegistrazioneControllerTest.java
@@ -0,0 +1,9 @@
+package it.unisa.c07.biblionet.registrazione.controller;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class RegistrazioneControllerTest {
+}
---
+++ src/test/java/it/unisa/c07/biblionet/registrazione/service/RegistrazioneServiceImplTest.java
@@ -0,0 +1,10 @@
+package it.unisa.c07.biblionet.registrazione.service;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class RegistrazioneServiceImplTest {
+}
