ba7241c3d94efd9ea24823842cd334c9a9c45caa
rayleighRe1 <69469718+rayleighRe1@users.noreply.github.com>
2020-12-21 16:49:14 +0100

    autenticazione.html + annotazioni controller

--- src/main/java/it/unisa/c07/biblionet/autenticazione/controller/AutenticazioneController.java
+++ src/main/java/it/unisa/c07/biblionet/autenticazione/controller/AutenticazioneController.java
@@ -1,4 +1,17 @@
 package it.unisa.c07.biblionet.autenticazione.controller;
 
+import it.unisa.c07.biblionet.autenticazione.service.AutenticazioneService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/autenticazione")
 public class AutenticazioneController {
+    private final AutenticazioneService autenticazioneService;
+
+    //@RequestMapping(value = "/", method = RequestMethod.POST)
+
 }
