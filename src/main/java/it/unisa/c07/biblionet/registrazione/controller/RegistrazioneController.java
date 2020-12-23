8c18d271d01ae81a805595e70466020babd3e341
BearGrillz99 <alessiocasolaro@hotmail.it>
2020-12-23 16:39:01 +0100

    Registrazione Biblioteca e testing funzionanti
    Co-Authored-By: Antonio  <57596737+atdepo@users.noreply.github.com>

--- src/main/java/it/unisa/c07/biblionet/registrazione/controller/RegistrazioneController.java
+++ src/main/java/it/unisa/c07/biblionet/registrazione/controller/RegistrazioneController.java
@@ -1,9 +1,12 @@
 package it.unisa.c07.biblionet.registrazione.controller;
 
 import it.unisa.c07.biblionet.model.entity.utente.Biblioteca;
+import it.unisa.c07.biblionet.model.entity.utente.Esperto;
+import it.unisa.c07.biblionet.model.entity.utente.Lettore;
 import it.unisa.c07.biblionet.registrazione.service.RegistrazioneService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,32 +23,41 @@ public class RegistrazioneController {
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String visualizzaRegistrazione() {
+
         return "registrazione";
     }
 
-    @RequestMapping(value = "/scegli", method = RequestMethod.GET)
-    public String scegliRegistrazione(final @RequestParam("scelta") String scelta) {
-        return "registrazione_"+scelta.toLowerCase();
+    @RequestMapping(value = "/scegli", method = RequestMethod.POST)
+    public String scegliRegistrazione(Model model,final @RequestParam("scelta") String scelta) {
+        model.addAttribute("esperto",new Esperto());
+        return "registrazione_" + scelta.toLowerCase();
+    }
+
+    @RequestMapping(value = "/lettore", method = RequestMethod.GET)
+    public String registrazioneLettore(final Lettore lettore) {
+        System.out.println(lettore);
+        return "registrazione_lettore";
     }
 
     @RequestMapping(value = "/biblioteca", method = RequestMethod.POST)
-    public String registrazioneBiblioteca(final Biblioteca biblioteca, final @RequestParam("conferma_password") String password){
+    public String registrazioneBiblioteca( final Biblioteca biblioteca,
+                                        final @RequestParam("conferma_password")
+                                                String password){
         try {
             MessageDigest md;
             md = MessageDigest.getInstance("SHA-256");
             byte[] arr = md.digest(password.getBytes());
             if(Arrays.compare(arr,biblioteca.getPassword())!=0){
-                System.out.println("La password non coincide\n");
+                System.out.println("Questa password non va bene");
                 return "registrazione_biblioteca";
             }
-            else{
-                this.registrazioneService.registraBiblioteca(biblioteca);
-                return "registrazione";//Questo redirect dovra portarci alla pagina Login
-            }
-
         } catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
         }
-        return null;
+
+        registrazioneService.registraBiblioteca(biblioteca);
+        return "registrazione";
+
     }
+
 }
--- src/main/java/it/unisa/c07/biblionet/registrazione/service/RegistrazioneService.java
+++ src/main/java/it/unisa/c07/biblionet/registrazione/service/RegistrazioneService.java
@@ -1,10 +1,18 @@
+
 package it.unisa.c07.biblionet.registrazione.service;
 
+import it.unisa.c07.biblionet.model.entity.Genere;
 import it.unisa.c07.biblionet.model.entity.utente.Biblioteca;
+import it.unisa.c07.biblionet.model.entity.utente.Esperto;
 import it.unisa.c07.biblionet.model.entity.utente.Lettore;
 import it.unisa.c07.biblionet.model.entity.utente.UtenteRegistrato;
+
+import java.util.List;
 
 public interface RegistrazioneService {
     UtenteRegistrato registraLettore(Lettore lettore);
     UtenteRegistrato registraBiblioteca(Biblioteca biblioteca);
+    UtenteRegistrato registraEsperto(Esperto utente);
+    Biblioteca findBibliotecaByEmail(String email);
+
 }
