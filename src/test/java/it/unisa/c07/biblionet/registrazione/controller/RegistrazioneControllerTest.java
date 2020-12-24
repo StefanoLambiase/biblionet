7560d163a13f05a468fbcc189b20675e2b9a05f0
Antonio <57596737+atdepo@users.noreply.github.com>
2020-12-24 01:07:31 +0100

    aggiunti ulteriori test
    Co-Authored-By: Alessio Casolaro <65906352+BearGrillz99@users.noreply.github.com>

--- src/test/java/it/unisa/c07/biblionet/registrazione/controller/RegistrazioneControllerTest.java
+++ src/test/java/it/unisa/c07/biblionet/registrazione/controller/RegistrazioneControllerTest.java
@@ -42,22 +42,16 @@ public final class RegistrazioneControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    /**
-     *
-     *
-     * @param esperto          Il lettore da registrare
-     * @param confermaPassword il campo conferma password del
-     *                         form per controllare
-     * @throws Exception Eccezione per MovkMvc
-     */
+
     /**
      * Metodo che testa la funzionalità gestita dal
      * controller per la registrazione di un esperto
      * avvenuta correttamente
      * simulando la richiesta http.
-     * @param esperto L'esperto da registrare
+     *
+     * @param esperto          L'esperto da registrare
      * @param confermaPassword la password da confermare
-     * @param emailBiblioteca la mail della biblioteca
+     * @param emailBiblioteca  la mail della biblioteca
      * @throws Exception Eccezione per MockMvc
      */
     @ParameterizedTest
@@ -65,8 +59,8 @@ public final class RegistrazioneControllerTest {
     @MethodSource("provideRegistrazioneEsperto")
     public void registrazioneEspertoBuonFine(
             final Esperto esperto, final String confermaPassword,
-            final String emailBiblioteca
-            ) throws Exception {
+            final String emailBiblioteca,
+            final String[] generi) throws Exception {
 
         Biblioteca biblioteca = new Biblioteca(
                 "bibliotecacarrisi@gmail.com",
@@ -95,7 +89,162 @@ public final class RegistrazioneControllerTest {
                 .param("via", esperto.getVia())
                 .param("recapito_telefonico", esperto.getRecapitoTelefonico())
                 .param("email_biblioteca", emailBiblioteca)
-                )
+                .param("genere", generi))
+                .andExpect(view().name("registrazione"));
+    }
+
+    /**
+     * Metodo che testa la funzionalità gestita dal
+     * controller per la registrazione di un esperto
+     * avvenuta in modo errato poiché la conferma della
+     * password è sbagliata simulando la richiesta http.
+     *
+     * @param esperto L'esperto da registrare
+     * @param confermaPassword la password da confermare
+     * @param emailBiblioteca  la mail della biblioteca
+     * @throws Exception Eccezione per MockMvc
+     */
+    @ParameterizedTest
+    @DisplayName("Registrazione Esperto che non va a buon fine " +
+                 "perché la conferma password è sbagliata")
+    @MethodSource("provideRegistrazioneEsperto")
+    public void registrazioneEspertoErrorePassword(
+            final Esperto esperto, final String confermaPassword,
+            final String emailBiblioteca,
+            final String[] generi) throws Exception {
+
+        Biblioteca biblioteca = new Biblioteca(
+                "bibliotecacarrisi@gmail.com",
+                "BibliotecaPassword",
+                "Napoli",
+                "Torre del Greco",
+                "Via Carrisi 47",
+                "1234567890",
+                "Biblioteca Carrisi"
+        );
+
+        when(registrazioneService.registraEsperto(new Esperto())).
+                thenReturn(esperto);
+        when(registrazioneService.findBibliotecaByEmail(emailBiblioteca)).
+                thenReturn(biblioteca);
+
+        this.mockMvc.perform(post("/registrazione/esperto")
+                .param("email", esperto.getEmail())
+                .param("nome", esperto.getNome())
+                .param("cognome", esperto.getCognome())
+                .param("username", esperto.getUsername())
+                .param("password", "EspertoPassword")
+                .param("conferma_password", "PASSWORDERRATA")
+                .param("provincia", esperto.getProvincia())
+                .param("citta", esperto.getCitta())
+                .param("via", esperto.getVia())
+                .param("recapito_telefonico", esperto.getRecapitoTelefonico())
+                .param("email_biblioteca", emailBiblioteca)
+                .param("genere", generi))
+                .andExpect(view().name("registrazione_esperto"));
+    }
+
+    /**
+     * Metodo che testa la funzionalità gestita dal
+     * controller per la registrazione di un esperto
+     * avvenuta correttamente
+     * simulando la richiesta http.
+     *
+     * @param esperto          L'esperto da registrare
+     * @param confermaPassword la password da confermare
+     * @param emailBiblioteca  la mail della biblioteca
+     * @throws Exception Eccezione per MockMvc
+     */
+    @ParameterizedTest
+    @DisplayName("Registrazione Esperto che non va a buon fine " +
+                 "perché la mail della biblioteca non corrisponde a " +
+                 "nessun utente")
+    @MethodSource("provideRegistrazioneEsperto")
+    public void registrazioneEspertoEmailBibliotecaErrata(
+            final Esperto esperto, final String confermaPassword,
+            final String emailBiblioteca,
+            final String[] generi) throws Exception {
+
+        Biblioteca biblioteca = new Biblioteca(
+                "bibliotecacarrisi@gmail.com",
+                "BibliotecaPassword",
+                "Napoli",
+                "Torre del Greco",
+                "Via Carrisi 47",
+                "1234567890",
+                "Biblioteca Carrisi"
+        );
+
+        when(registrazioneService.registraEsperto(new Esperto())).
+                thenReturn(esperto);
+        when(registrazioneService.findBibliotecaByEmail(emailBiblioteca)).
+                thenReturn(null);
+
+        this.mockMvc.perform(post("/registrazione/esperto")
+                .param("email", esperto.getEmail())
+                .param("nome", esperto.getNome())
+                .param("cognome", esperto.getCognome())
+                .param("username", esperto.getUsername())
+                .param("password", "EspertoPassword")
+                .param("conferma_password", confermaPassword)
+                .param("provincia", esperto.getProvincia())
+                .param("citta", esperto.getCitta())
+                .param("via", esperto.getVia())
+                .param("recapito_telefonico", esperto.getRecapitoTelefonico())
+                .param("email_biblioteca", emailBiblioteca)
+                .param("genere", generi))
+                .andExpect(view().name("registrazione_esperto"));
+    }
+
+    /**
+     * Metodo che testa la funzionalità gestita dal
+     * controller per la registrazione di un esperto
+     * avvenuta correttamente
+     * simulando la richiesta http.
+     *
+     * @param esperto          L'esperto da registrare
+     * @param confermaPassword la password da confermare
+     * @param emailBiblioteca  la mail della biblioteca
+     * @throws Exception Eccezione per MockMvc
+     */
+    @ParameterizedTest
+    @DisplayName("Registrazione Esperto che va a buon fine anche se i generi " +
+                 "passati sono null")
+    @MethodSource("provideRegistrazioneEsperto")
+    public void registrazioneEspertoBuonFineGeneriNull(
+            final Esperto esperto, final String confermaPassword,
+            final String emailBiblioteca) throws Exception {
+
+        Biblioteca biblioteca = new Biblioteca(
+                "bibliotecacarrisi@gmail.com",
+                "BibliotecaPassword",
+                "Napoli",
+                "Torre del Greco",
+                "Via Carrisi 47",
+                "1234567890",
+                "Biblioteca Carrisi"
+        );
+
+        String [] generi = {""};
+
+        when(registrazioneService.registraEsperto(new Esperto())).
+                thenReturn(esperto);
+        when(registrazioneService.findBibliotecaByEmail(emailBiblioteca)).
+                thenReturn(biblioteca);
+
+        this.mockMvc.perform(post("/registrazione/esperto")
+                .param("email", esperto.getEmail())
+                .param("nome", esperto.getNome())
+                .param("cognome", esperto.getCognome())
+                .param("username", esperto.getUsername())
+                .param("password", "EspertoPassword")
+                .param("conferma_password", confermaPassword)
+                .param("provincia", esperto.getProvincia())
+                .param("citta", esperto.getCitta())
+                .param("via", esperto.getVia())
+                .param("recapito_telefonico", esperto.getRecapitoTelefonico())
+                .param("email_biblioteca", emailBiblioteca)
+                .param("genere", generi))
                 .andExpect(view().name("registrazione"));
     }
 
@@ -106,6 +255,7 @@ public final class RegistrazioneControllerTest {
      * @return Lo stream di dati.
      */
     private static Stream<Arguments> provideRegistrazioneEsperto() {
+        String[] generi = {"Fantasy", "Politico"};
 
         return Stream.of(
                 Arguments.of(
@@ -120,7 +270,8 @@ public final class RegistrazioneControllerTest {
                                 "Elia",
                                 "Viviani",
                                 null
-                        ), "EspertoPassword", "bibliotecacarrisi@gmail.com"
+                        ), "EspertoPassword", "bibliotecacarrisi@gmail.com",
+                        generi
                 )
         );
 
@@ -128,9 +279,10 @@ public final class RegistrazioneControllerTest {
 
     /**
      * Test che registra correttamente una biblioteca.
-     * @param biblioteca la biblioteca da registrare
-     * @param confermaPassword la passoword da confermare
-     * @throws Exception Eccezzione di MockMvc
+     *
+     * @param biblioteca       la biblioteca da registrare
+     * @param confermaPassword la password da confermare
+     * @throws Exception Eccezione di MockMvc
      */
     @ParameterizedTest
     @DisplayName("Registrazione Biblioteca che va a buon fine")
@@ -153,24 +305,82 @@ public final class RegistrazioneControllerTest {
                         biblioteca.getRecapitoTelefonico()))
                 .andExpect(view().name("registrazione"));
     }
+
+    /**
+     * Test che non registra correttamente una biblioteca.
+     * poiché la conferma della password è errata
+     *
+     * @param biblioteca       la biblioteca da registrare
+     * @param confermaPassword la passoword da confermare
+     * @throws Exception Eccezione di MockMvc
+     */
+    @ParameterizedTest
+    @DisplayName("Registrazione Biblioteca che non va a buon fine " +
+            "perché la conferma password è sbagliata")
+    @MethodSource("provideRegistrazioneBiblioteca")
+    public void registrazioneBibliotecaPasswordErrata(
+                                                final Biblioteca biblioteca,
+                                                final String confermaPassword)
+                                                            throws Exception {
+
+        when(registrazioneService.registraBiblioteca(new Biblioteca()))
+                .thenReturn(biblioteca);
+
+        this.mockMvc.perform(post("/registrazione/biblioteca")
+                .param("email", biblioteca.getEmail())
+                .param("nomeBiblioteca", biblioteca.getNomeBiblioteca())
+                .param("password", "BibliotecaPassword")
+                .param("conferma_password", "PASSWORDSBAGLIATA")
+                .param("provincia", biblioteca.getProvincia())
+                .param("citta", biblioteca.getCitta())
+                .param("via", biblioteca.getVia())
+                .param("recapito_telefonico",
+                        biblioteca.getRecapitoTelefonico()))
+                .andExpect(view().name("registrazione_biblioteca"));
+    }
+
+
+    /**
+     * Restituisce i dati per la registrazione della biblioteca.
+     *
+     * @return i dati per il testing
+     */
+    private static Stream<Arguments> provideRegistrazioneBiblioteca() {
+
+        return Stream.of(Arguments.of(
+                new Biblioteca(
+                        "bibliotecacarrisi@gmail.com",
+                        "BibliotecaPassword",
+                        "Napoli",
+                        "Torre del Greco",
+                        "Via Carrisi 47",
+                        "1234567890",
+                        "Biblioteca Carrisi"
+                ),
+                "BibliotecaPassword"//Password Conferma
+        ));
+    }
+
+
     /**
      * controller per la registrazione di un lettore
      * avvenuta correttamente
      * simulando la richiesta http.
-     * @param lettore Il lettore da registrare
+     *
+     * @param lettore          Il lettore da registrare
      * @param confermaPassword il campo conferma password del
-     * form per controllare
+     *                         form per controllare
      * @throws Exception Eccezione per MovkMvc
      */
     @ParameterizedTest
-    @DisplayName("Registrazione che va a buon fine")
+    @DisplayName("Registrazione Lettore che va a buon fine")
     @MethodSource("provideRegistrazioneLettore")
     public void registrazioneLettoreBuonFine(final Lettore lettore,
                                              final String confermaPassword)
             throws Exception {
 
         when(registrazioneService.registraLettore(new Lettore()))
-                                        .thenReturn(lettore);
+                .thenReturn(lettore);
 
         this.mockMvc.perform(post("/registrazione/lettore")
                 .param("email", lettore.getEmail())
@@ -191,17 +401,18 @@ public final class RegistrazioneControllerTest {
      * controller per la registrazione di un lettore
      * avvenuta in modo scorretto
      * simulando la richiesta http.
-     * @param lettore Il lettore da registrare
+     *
+     * @param lettore          Il lettore da registrare
      * @param confermaPassword il campo conferma password del
-     * form per controllare
+     *                         form per controllare
      * @throws Exception Eccezione per MovkMvc
      */
     @ParameterizedTest
-    @DisplayName("Registrazione che non va a buon fine, "
-            + "password e conferma password sbagliate")
+    @DisplayName("Registrazione Lettore che non va a buon fine, "
+               + "password e conferma password sbagliate")
     @MethodSource("provideRegistrazioneLettore")
     public void registrazioneLettoreErrataPassword(final Lettore lettore,
-                                     final String confermaPassword)
+                                                   final String confermaPassword)
             throws Exception {
 
         when(registrazioneService.registraLettore(new Lettore()))
@@ -224,13 +435,14 @@ public final class RegistrazioneControllerTest {
     /**
      * Simula i dati inviati da un metodo
      * http attraverso uno stream.
+     *
      * @return Lo stream di dati.
      */
     private static Stream<Arguments> provideRegistrazioneLettore() {
 
         return Stream.of(
                 Arguments.of(
-                         new Lettore(
+                        new Lettore(
                                 "giuliociccione@gmail.com",
                                 "LettorePassword",
                                 "Salerno",
@@ -243,25 +455,5 @@ public final class RegistrazioneControllerTest {
                         ), "LettorePassword"//Password Conferma
                 )
         );
-    }
-
-    /**
-     * Restituisce i dati per la registrazione della biblioteca.
-     * @return i dati per il testing
-     */
-    private static Stream<Arguments> provideRegistrazioneBiblioteca() {
-
-        return Stream.of(Arguments.of(
-                new Biblioteca(
-                        "bibliotecacarrisi@gmail.com",
-                        "BibliotecaPassword",
-                        "Napoli",
-                        "Torre del Greco",
-                        "Via Carrisi 47",
-                        "1234567890",
-                        "Biblioteca Carrisi"
-                ),
-                "BibliotecaPassword"//Password Conferma
-        ));
     }
 }
