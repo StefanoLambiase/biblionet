0dd72e163262e98b18cd7db21d848f7bfc526758
BearGrillz99 <alessiocasolaro@hotmail.it>
2021-01-15 18:57:42 +0100

    Implementazione Error Handler

---
+++ src/main/java/it/unisa/c07/biblionet/utils/GlobalControllerExceptionHandler.java
@@ -0,0 +1,16 @@
+package it.unisa.c07.biblionet.utils;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+class GlobalControllerExceptionHandler {
+    @ResponseStatus(HttpStatus.NOT_FOUND)  // 409
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public void handleConflict() {
+        // Nothing to do
+    }
+}
